5116a9c67838cc8c87e5187d6c1590e18ca2aada
Davi Silva <davi95silvasouza12@gmail.com>
2026-03-02 07:39:56 -0300

    feat: adiciona tratamento de excecoes com try-catch

--- src/main/java/br/com/gerenciador/Main.java
+++ src/main/java/br/com/gerenciador/Main.java
@@ -4,6 +4,7 @@ import br.com.gerenciador.model.Personagem;
 import br.com.gerenciador.repository.BauGenerico;
 import br.com.gerenciador.service.FiltroService;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -36,13 +37,15 @@ public class Main {
         servico.listarPorCategoria(meuBau.buscarTodos(), escolha);
 
 
+        try {
+            System.out.println("Digite o valor de pontuação que deseja mínimo:");
+            pontuacao = scanner.nextInt();
 
-        System.out.println("Digite o valor de pontuação que deseja mínimo:");
-        pontuacao = scanner.nextInt();
-
-
-        System.out.println("--- Buscando Por Pontuações  ---");
-        servico.listarPorPontuacaoMinima(meuBau.buscarTodos(), pontuacao);
+            System.out.println("--- Buscando Por Pontuações  ---");
+            servico.listarPorPontuacaoMinima(meuBau.buscarTodos(), pontuacao);
+        }catch(InputMismatchException erroDigitacao){
+            System.out.println("Ops! Você deve digitar apenas números.");
+        } ;
 
 
 
