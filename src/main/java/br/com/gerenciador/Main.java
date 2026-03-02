7c469b34cc359d4c5adb5e4e7a89363b5e91f0a3
Davi Silva <davi95silvasouza12@gmail.com>
2026-03-02 08:33:54 -0300

    feat: implementa menu interativo e aprimora tratamento de excecoes

--- src/main/java/br/com/gerenciador/Main.java
+++ src/main/java/br/com/gerenciador/Main.java
@@ -11,44 +11,78 @@ public class Main {
     public static void main(String[] args) {
         String escolha;
         int pontuacao;
-
+        int menu = -1;
 
         Scanner scanner = new Scanner(System.in);
 
+        FiltroService servico = new FiltroService();
 
-        System.out.println("Digite o nome da categoria do personagem:");
-        escolha = scanner.nextLine();
-
-
+        //Inicializando e criando "bau" e personagem
         BauGenerico<Personagem> meuBau = new BauGenerico<>();
+
         Personagem p1 = new Personagem("Lion", "S.T.A.R.S", 1000);
         Personagem p2 = new Personagem("Adan", "AGENTE DUPLO", 750);
         Personagem p3 = new Personagem("Salazhar", "SANDLER", 600);
         Personagem p4= new Personagem("Krauzer", "UMBRELLA", 800);
-
+        // salvando no "bau"
         meuBau.salvar(p1);
         meuBau.salvar(p2);
         meuBau.salvar(p3);
         meuBau.salvar(p4);
 
-        FiltroService servico = new FiltroService();
-        System.out.println("--- Buscando personagens  ---");
-
-        servico.listarPorCategoria(meuBau.buscarTodos(), escolha);
 
 
-        try {
-            System.out.println("Digite o valor de pontuação que deseja mínimo:");
-            pontuacao = scanner.nextInt();
+        while(menu != 0) {
 
-            System.out.println("--- Buscando Por Pontuações  ---");
-            servico.listarPorPontuacaoMinima(meuBau.buscarTodos(), pontuacao);
-        }catch(InputMismatchException erroDigitacao){
-            System.out.println("Ops! Você deve digitar apenas números.");
-        } ;
+            System.out.println("--- MENU DA BIBLIOTECA ---");
+            System.out.println("1 - Buscar por Categoria");
+            System.out.println("2 - Buscar por Pontuação Minima");
+            System.out.println("0 - Sair");
+
+            try{
+                menu = scanner.nextInt();
+                scanner.nextLine(); //limpando o teclado
+
+                if(menu == 1){
+                    System.out.println("Digite o nome da categoria do personagem:");
+                    escolha = scanner.nextLine();
 
 
+                    System.out.println("--- Buscando personagens  ---");
+
+                    servico.listarPorCategoria(meuBau.buscarTodos(), escolha);
+                }
+
+                else if (menu == 2){
+
+                    try {
+                        System.out.println("Digite o valor de pontuação que deseja mínimo:");
+                        pontuacao = scanner.nextInt();
+                        scanner.nextLine();
+
+                        System.out.println("--- Buscando Por Pontuações  ---");
+                        servico.listarPorPontuacaoMinima(meuBau.buscarTodos(), pontuacao);
+                    } catch (InputMismatchException erroDigitacao) {
+                        System.out.println("Ops! Você deve digitar apenas números.");
+                        scanner.nextLine();
+                    }
+
+                } else if (menu == 0) {
+                    System.out.println("Saindo do sistema... Até logo!");
+                }else{
+                    System.out.println("Opção inválida! Tente novamente");
+                }
+            }catch (InputMismatchException erroDigitacao){
+                System.out.println("Ops! Você deve digitar apenas números. Tente novamente");
+                scanner.nextLine();
+            }
+
+
+
+        }
 
 
     }
+
+
 }
